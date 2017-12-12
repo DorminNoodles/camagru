bf432949348bd3246b7ab3fb292dc99198116d4c
Loic CHETY <lchety@e2r12p23.42.fr>
2017-12-12 19:26:06 +0100

    research

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <?php
 // echo getcwd();
 
-  require_once('config/database.php');
-  require_once('config/setup.php');
+
+$mysqli = new mysqli('localhost', 'root', 'qwerty');
+
+if ($mysqli->connect_errno)
+{
+  echo "REDRUM";
+}
+else
+{
+  echo "CA MARCHE TA MERE";
+}
+
+if ($mysqli->query("CREATE DATABASE databasename") === TRUE)
+{
+    printf("Table myCity créée avec succès.\n");
+}
+
+
+
+// $mysqli = new mysqli("localhost", "root", "qwerty", "world");
+//
+// /* Vérification de la connexion */
+// if ($mysqli->connect_errno) {
+//     printf("Connect failed: %s\n", $mysqli->connect_error);
+//     exit();
+// }
+
+
+  // require_once('config/database.php');
+  // require_once('config/setup.php');
 
 
 ?>
