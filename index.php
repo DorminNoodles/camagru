407f3107f327a31df98c11d002522a9e812e90dd
Dormin <loic.chety@gmail.com>
2017-12-13 21:10:19 +0100

    wip

--- index.php
+++ index.php
@@ -1,36 +1,13 @@
 <?php
-// echo getcwd();
 
+echo 'hoho';
 
-$mysqli = new mysqli('localhost', 'root', 'qwerty');
-
-if ($mysqli->connect_errno)
+if (isset($_GET['url']))
 {
-  echo "REDRUM";
-}
-else
-{
-  echo "CA MARCHE TA MERE";
+	echo $_GET['url'];
 }
 
-if ($mysqli->query("CREATE DATABASE databasename") === TRUE)
-{
-    printf("Table myCity créée avec succès.\n");
-}
-
-
-
-// $mysqli = new mysqli("localhost", "root", "qwerty", "world");
-//
-// /* Vérification de la connexion */
-// if ($mysqli->connect_errno) {
-//     printf("Connect failed: %s\n", $mysqli->connect_error);
-//     exit();
-// }
-
-
-  // require_once('config/database.php');
-  // require_once('config/setup.php');
-
+echo '<br />';
+echo $_SERVER['REQUEST_URI'];
 
 ?>
