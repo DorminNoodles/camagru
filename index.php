49a1afeb17ebdce39fbecb3601aa9558bd920a6c
Loic CHETY <lchety@e2r12p11.42.fr>
2017-12-14 10:49:02 +0100

    wip

--- index.php
+++ index.php
@@ -2,21 +2,31 @@
 // echo getcwd();
 
 
-$mysqli = new mysqli('localhost', 'root', 'qwerty');
+// $mysqli = new mysqli('localhost', 'root', 'qwerty');
 
-if ($mysqli->connect_errno)
-{
-  echo "REDRUM";
-}
-else
-{
-  echo "CA MARCHE TA MERE";
-}
+// $strConnection = 'mysql:host=localhost';
 
-if ($mysqli->query("CREATE DATABASE databasename") === TRUE)
-{
-    printf("Table myCity créée avec succès.\n");
-}
+// $pdo = new PDO($connStr, 'Utilisateur', 'Mot de passe', $arrExtraParam);
+
+$pdo = new PDO('mysql:host=localhost;', 'root', 'qwerty');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = 'CREATE DATABASE pikmin';
+$return = $pdo->exec($query);
+
+// if ($mysqli->connect_errno)
+// {
+//   echo "REDRUM";
+// }
+// else
+// {
+//   echo "CA MARCHE TA MERE";
+// }
+//
+// if ($mysqli->query("CREATE DATABASE hello") === TRUE)
+// {
+//     printf("Table myCity créée avec succès.\n");
+// }
 
 
 
